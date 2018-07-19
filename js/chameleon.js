42d8c8d8c46d6e53db85800282c19071d8740a82
Marcus Battle <themarcusbattle@marcbook.local>
2018-07-18 20:28:26 -0400

    Added JS

---
+++ js/chameleon.js
@@ -0,0 +1,37 @@
+let chameleonMoveSlideLeft = ( event ) => {
+
+    const slides = document.querySelectorAll('.slide')
+    const activeSlide = document.querySelectorAll('.slide.active')
+
+    slides.forEach( ( slide, index ) => {
+
+        let slideStyles   = getComputedStyle( slide )
+        let slideMargin   = slideStyles.marginLeft.replace( 'px', '' ) * 2
+        let slideWidth    = slide.offsetWidth + slideMargin
+        let slidePosition = slide.style.left.replace( 'px', '' ) * 1
+
+        console.log( slidePosition )
+        console.log( slideWidth )
+
+        slide.style.left = slidePosition - slideWidth
+    } )
+}
+
+let chameleonMoveSlideRight = ( event ) => {
+
+    const slides = document.querySelectorAll('.slide')
+    const activeSlide = document.querySelectorAll('.slide.active')
+
+    slides.forEach( ( slide, index ) => {
+
+        let slideStyles   = getComputedStyle( slide )
+        let slideMargin   = slideStyles.marginLeft.replace( 'px', '' ) * 2
+        let slideWidth    = slide.offsetWidth + slideMargin
+        let slidePosition = slide.style.left.replace( 'px', '' ) * 1
+
+        console.log(  )
+        console.log( slideWidth )
+
+        slide.style.left = slidePosition + slideWidth
+    } )
+}
